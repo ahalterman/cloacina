229648e39697dc29aef12875efb7c964479b5252
Andy <ahalterman0@gmail.com>
2015-08-28 12:57:45 -0400

    Don't mess up article formatting. Closes #1

--- lexikon/extract_from_b64.py
+++ lexikon/extract_from_b64.py
@@ -4,11 +4,12 @@ import json
 def extract_from_b64(encoded_doc):
     #doc = base64.urlsafe_b64decode(encoded_doc)
     doc = encoded_doc.decode("base64")
+    doc = re.sub("</p><p>", " ", doc)
     soup = BeautifulSoup(doc)
     news_source = soup.find("meta", {"name":"sourceName"})['content']
-    article_title = soup.find("title").text
-    publication_date = soup.find("div", {"class":"PUB-DATE"}).text
-    article_body = soup.find("div", {"class":"BODY"}).text
+    article_title = soup.find("title").text.strip()
+    publication_date = soup.find("div", {"class":"PUB-DATE"}).text.strip()
+    article_body = soup.find("div", {"class":"BODY"}).text.strip()
     doc_id = soup.find("meta", {"name":"documentToken"})['content']
 
     data = {"news_source" : news_source,
